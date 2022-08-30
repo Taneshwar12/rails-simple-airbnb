cda852a81b52abf65e31b1688e2ea386c0414d2c
Taneshwar Sodhi <taneshwar12@gmail.com>
2022-08-30 17:58:09 +1000

    In progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Random flat',
+  address: '1 Austrlian road',
+  description: 'Nice place in Australia',
+  price_per_night: 80,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Random place 2',
+  address: '11 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 175,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Random place 3',
+  address: '12 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 200,
+  number_of_guests: 7
+)
